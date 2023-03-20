23b2c87ae3fc7f2c199df82f7f59a3cd489c81b3
adinakussainova <kusainova.adina@gmail.com>
2023-03-20 13:34:30 +0600

    second_com

--- authController.js
+++ authController.js
@@ -56,14 +56,6 @@ class authController {
         }
     }
 
-    async getUsers(req, res) {
-        try {
-            const users = await User.find()
-            res.json(users)
-        } catch (e) {
-            console.log(e)
-        }
-    }
 }
 
 module.exports = new authController()
